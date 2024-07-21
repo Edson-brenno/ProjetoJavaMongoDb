5e5cf4f7b4d5054ca89c3899b3b2057f7dd97b92
brenno <brennorebeiro@gmail.com>
2024-07-21 17:26:32 -0300

    Add user service

--- src/main/java/com/spring/Repository/UserRepository.java
+++ src/main/java/com/spring/Repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.spring.Repository;
 
-import org.apache.catalina.User;
+import com.spring.Dominio.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
---
+++ src/main/java/com/spring/Service/UserService.java
@@ -0,0 +1,18 @@
+package com.spring.Service;
+
+import com.spring.Repository.UserRepository;
+import com.spring.Dominio.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
