e3fe9b1c888c0722485ddad74fedc670e75d967a
brenno <brennorebeiro@gmail.com>
2024-07-21 17:53:39 -0300

    Add config

---
+++ src/main/java/com/spring/Config/HomologacaoConfig.java
@@ -0,0 +1,27 @@
+package com.spring.Config;
+
+import com.spring.Dominio.User;
+import com.spring.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("Homologacao")
+public class HomologacaoConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        //Ading users
+        userRepository.save(new User("John", "Doe1"));
+        userRepository.save(new User("Jane", "Doe2"));
+        userRepository.save(new User("Jack", "Doe3"));
+        userRepository.save(new User("Caio", "Doe4"));
+    }
+}
--- src/main/java/com/spring/workshopcommongo/WorkShopComMongoApplication.java
+++ src/main/java/com/spring/workshopcommongo/WorkShopComMongoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
