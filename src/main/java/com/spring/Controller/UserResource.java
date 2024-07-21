dfba9b188ee4d3837868b0aa4ea80ac5b3ae88e9
brenno <brennorebeiro@gmail.com>
2024-07-21 18:29:37 -0300

    Change The user resource method get

--- src/main/java/com/spring/Controller/UserResource.java
+++ src/main/java/com/spring/Controller/UserResource.java
@@ -3,9 +3,14 @@ package com.spring.Controller;
 import com.spring.Dominio.User;
 import com.spring.Dto.UserDto;
 import com.spring.Dto.UserRegisterDto;
+import com.spring.Dto.ReponseStructDto;
 import com.spring.Repository.UserRepository;
 import com.spring.Service.UserService;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -15,12 +20,18 @@ import java.util.List;
 public class UserResource {
     @Autowired
     private UserService userService;
+    @Autowired
+    private ServletContext servletContext;
 
     @GetMapping
-    public ResponseEntity<List<UserDto>> getUsers() {
+    public ResponseEntity<ReponseStructDto<List<UserDto>>> getUsers(HttpServletRequest request) {
         List<User> users = userService.findAll();
         List<UserDto> usersDto = users.stream().map(UserDto::new).toList();
-        return ResponseEntity.ok().body(usersDto);
+
+        ReponseStructDto<List<UserDto>> reponse = new ReponseStructDto<>(
+                HttpStatus.OK, usersDto, "Usuários obtidos com sucesso", request.getRequestURI()
+        );
+        return ResponseEntity.ok().body(reponse);
     }
 
     @PostMapping
