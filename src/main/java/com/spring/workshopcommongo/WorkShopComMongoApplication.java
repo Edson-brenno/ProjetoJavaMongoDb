62ebfd63effe8a1c545c7152ed9a731d9fb8eb2a
brenno <brennorebeiro@gmail.com>
2024-07-21 16:57:59 -0300

    Start project

---
+++ src/main/java/com/spring/workshopcommongo/WorkShopComMongoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.workshopcommongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopComMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkShopComMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/workshopcommongo/WorkShopComMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.workshopcommongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopComMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
